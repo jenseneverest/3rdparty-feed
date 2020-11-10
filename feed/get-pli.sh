f9436d861ee16d2b7a71237e01f9536908a9c45a
Persian Prince <persianpros@yahoo.com>
2020-11-10 21:39:13 +0330

    Remove Dima plugins as we compile them from the sources now

--- feed/get-pli.sh
+++ feed/get-pli.sh
@@ -2,7 +2,7 @@
 
 mkdir -p pli
 cd pli
-wget http://downloads.openpli.org/feeds/openpli-7-release/3rd-party/Packages.gz
+wget http://downloads.openpli.org/feeds/openpli-8-release/3rd-party/Packages.gz
 gzip -d Packages.gz
 find -type f -name "*Packages*" | xargs -L1 sed -i '/^Filename: /p'
 sed -n '/^Filename: /p' Packages > Packages-files
